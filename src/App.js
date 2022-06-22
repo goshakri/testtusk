4027a32ce814eac57f49b118104e6a21d135f3d2
George <george@XPUAKYIW0056.local>
2022-06-22 23:53:07 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import group from "./images/group.png";
 
 function App() {
   return (
-    <div></div>
+    <div className="Page">
+      <header>
+        <div className="Section-content">
+          <nav className="SiteNavigation">
+
+            <ul className="SiteNavigation-column">
+             <li className="SiteNavigation-item">
+                <img className='SiteNavigation-uperItem' src={group} alt="" />
+                <h1 className='SiteNavigation-uperItem'>testtask</h1>
+              </li>
+            </ul>
+            <ul className="SiteNavigation-column">
+              <li className="SiteNavigation-item">
+                <a className="SiteNavigation-link" href="/">
+                  Users
+                </a>
+              </li>
+              <li className="SiteNavigation-item">
+                <a className="SiteNavigation-link" href="/">
+                  Sign Up
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 }
 
